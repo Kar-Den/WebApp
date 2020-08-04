70d4682a8e292577006ae90e413a0f70b1f41dcc
Kar-Den <k.denis.vlad@gmail.com>
2020-08-05 02:48:44 +0300

    Webapp подготовленно

---
+++ src/main/java/it/kours/MyFirstServlet.java
@@ -0,0 +1,18 @@
+package it.kours;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "MyFirstServlet", urlPatterns = "")
+public class MyFirstServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.getWriter().println("<h1>privet<h1>");
+
+    }
+}
